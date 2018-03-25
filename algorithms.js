76bfc445aba2337f07dc9ae69e99e8184f0963b3
Paris Ward <wardparis.pw@gmail.com>
2018-03-25 15:53:01 -0700

    more algorithm practice

--- algorithms.js
+++ algorithms.js
@@ -20,16 +20,57 @@ function spinalCase(str) {
 
 //Sum all Odd Fibonacci Numbers
 function sumFibs(num) {
-  //Create an empty array to push odd numbers
-  var odds = [];
-  //Loop through num
-  for(var i = 0; i<num.length; i++) {
-    var currentNumber = i;
-    currentNumber += i;
-    //Check if the sum is even
-    if(i % 2 == 0) {
-     
+  //Create variables to keep track of the current and previous numbers
+  var prevNumber = 0;
+  var currNumber = 1;
+  var result = 0;
+  //Iterate through num using a while loop
+    while(currNumber <= num) {
+      //Check if the current number is even
+      if(currNumber % 2 !== 0) {
+        //If even, add it to our result variable
+        result += currNumber;
+      }
+      //Get the next number and swap values after
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+      
     }
-  }
-  return num;
+  return result;
 }
+
+
+//Sum all Primes
+function sumPrimes(num) {
+  var res = 0;
+  //Create a function to find the prime numbers up to 'max'
+  function findPrimes(max) {
+    //Create variables for our loops and empty arrays to .push to
+    var sieve = [];
+    var i;
+    var j;
+    var primes = [];
+    //Loop through max starting at 2(first prime number) 
+    for(i = 2; i <= max; ++i) {
+      //If not(!) a sieve number..
+      if(!sieve[i]) {
+        //Push i to the primes array
+        primes.push(i);
+        
+        for(j = i << 1; j<=max; j+=i) {
+          //add other numbers(non primes) to sieve array
+          sieve[j] = true;
+        }
+      }
+    }
+    return primes;
+  }
+  //Add the primes
+  var primes = findPrimes(num); 
+    for(var p = 0; p < primes.length; p++) {
+      res += primes[p];
+    }
+  
+    return res;
+}
+
