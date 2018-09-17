27db149f9ccf4dcb7cd0e9d3839eb155422e9592
Paris Ward <wardparis.pw@gmail.com>
2018-09-17 09:09:23 -0700

    more algos

--- algorithms.js
+++ algorithms.js
@@ -182,3 +182,19 @@ function truthCheck(collection, pre) {
   //check to see if it is truthy and return true or false
   return counter == collection.length;
 }
+
+//FrankenSplice
+//Use array methods slice and splice to copy each element
+//of the first array into the second array, in order
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  let copy = arr2.slice();
+
+  for(let i = 0; i < arr1.length; i++) {
+    copy.splice(n, 0 , arr1[i]);
+    n++
+  }
+  return copy;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
